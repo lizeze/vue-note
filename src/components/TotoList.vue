258f079f4dd1970734d124cb8a4802458a1207af
zeze.li <lizeze.boy@gmail.com>
2020-11-19 23:29:37 +0800

    todo list

---
+++ src/components/TotoList.vue
@@ -0,0 +1,39 @@
+<template>
+
+     <div>
+
+         <ul>
+
+             <li v-for="item in this.list":key="item.key">
+
+                  {{item.text}} <button @click="deleteHandle(item.key)">删除</button>
+             </li>
+         </ul>
+
+     </div>
+</template>
+
+<script>
+    export default {
+        name: 'TotoList',
+        props:{
+
+            list:{
+                type:Array,
+                default(){
+                    return[]
+                }
+            }
+        },
+        methods:{
+            deleteHandle:function (key) {
+                 this.$emit('deleteHandle',key)
+            }
+        }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Todo.vue
@@ -0,0 +1,40 @@
+<template>
+
+    <div>
+        <input type="text" v-model="inputText">
+        <button @click="addTodo">增加</button>
+        <toto-list :list="list" @deleteHandle="deleteHandle"></toto-list>
+
+    </div>
+</template>
+
+<script>
+    import TotoList from "../components/TotoList";
+
+    export default {
+        name: 'todo',
+        components: {TotoList},
+        data: () => {
+            return {
+                inputText: '',
+                list: []
+            }
+        },
+        methods: {
+            addTodo: function () {
+                this.list.push({
+                    key: Date.now(),
+                    text: this.inputText
+                })
+                this.inputText=''
+            },
+            deleteHandle: function (key) {
+                this.list = this.list.filter(t => t.key != key)
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
